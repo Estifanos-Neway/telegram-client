ccd5837488eeeb209a4853250bd1abefae0d7af4
Estifanos Neway <estifanos.neway.d@gmail.com>
2022-02-18 17:17:54 +0300

    known ids loger added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { Airgram, Auth, prompt, toObject } from 'airgram'
 import 'dotenv/config';
-import { KnownChatIds, myGender } from './variables.js';
+import { myGender, knownChatsIdentifier, knownChatIds } from './variables.js';
 
 //local variables
 let thisUserName, thisUserPhone, thisUserId;
@@ -14,13 +14,13 @@ const airgram = new Airgram({
 })
 
 //function declarations
-function newMessageHandler(ctx, next) {
+async function newMessageHandler(ctx, next) {
     let message = ctx.update.message;
     if(!message.isOutgoing){
         let chatId = message.chatId;
-        if(chatId in KnownChatIds){
-            let senderName = KnownChatIds[chatId];
-            let newMessageText = `Hi ${senderName}, I am ${thisUserName}s bot. ${thisUserName} is not around at the moment, but ${thisUserGenderPronoun} will comeback soon. Thank you 😊`
+        if(chatId in knownChatIds){
+            let senderName = knownChatIds[chatId];
+            let newMessageText = `Hi ${senderName}, I am ${thisUserName}'s bot. ${(thisUserName.slice(0,1)).toUpperCase()}${thisUserName.slice(1)} is not around at the moment, but ${thisUserGenderPronoun} will comeback soon. Thank you 😊`
             let SendMessageParams = {
                 chatId: chatId,
                 inputMessageContent: {
@@ -32,7 +32,7 @@ function newMessageHandler(ctx, next) {
                 }
             }
 
-            console.log(`Known Chat from ${KnownChatIds[chatId]}.`)
+            console.log(`Known Chat from ${knownChatIds[chatId]}.`)
             airgram.api.sendMessage(SendMessageParams).then(
                 (result) => {
                     let responseType = result.response._;
@@ -49,6 +49,26 @@ function newMessageHandler(ctx, next) {
                 }
             )
         };
+    } else{
+        if(message.content._ == "messageText"){
+            if(message.content.text.text.startsWith(knownChatsIdentifier)){
+                let messageId = message.id;
+                let chatId = message.chatId;
+                // await airgram.api.getChat(chatId);
+                // airgram.api.deleteMessages(chatId,[messageId],true).then(
+                //     (result)=>{
+                //         console.log("[result]: ",result);
+                //     }
+                // ).catch(
+                //     (error)=>{
+                //         console.error("[error]: ",error);
+                //     }
+                // )
+
+                let receiversName = message.content.text.text.slice(1);
+                console.log(`${chatId}:${receiversName}`)
+            }
+        }
     }
     return next();
 }
@@ -64,7 +84,7 @@ airgram.on("updateNewMessage",newMessageHandler);
 // executions starts here
 airgram.api.getMe().then((me)=>{
     thisUserId = me.response.id;
-    thisUserName = KnownChatIds[thisUserId]??me.response.firstName;
+    thisUserName = knownChatIds[thisUserId]??me.response.firstName;
     thisUserPhone = me.response.phoneNumber;
     console.log(`Hey ${thisUserName}, (Phone: +${thisUserPhone} | Id: ${thisUserId}).`);
 })
--- variables.js
+++ variables.js
@@ -1,3 +1,4 @@
 // global variables
-export const KnownChatIds = {391983541 : "estif"};
-export const myGender = "female";
+export const myGender = "male";
+export const knownChatsIdentifier = ".";
+export const knownChatIds = {1398598219:"kely", 391983541 : "estif", 452703273:"miku", 308921082 : "kalsh"};
